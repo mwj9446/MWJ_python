c832a8e189e4ddc64d4892fecf98cd8789dd4441
mai <10086>
2020-01-05 11:53:16 +0800

    q

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^u*/', include('users.urls')),
+    url(r'^u*/', include('users.urls', 'login.urls')),
 ]
--- demo/login/urls.py
+++ demo/login/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^i*/$', views.index),
-    # url(r'^d*/$', include('my.urls')),
+    url(r'^d*/$', views.index),
 ]
